41073eab9474624dae146b776042fc53c0cc0038
EuroCent <maik.schwieger@yahoo.com>
2026-05-29 04:07:34 +0200

    release: v1.1.24

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 author 'EuroCent'
 name 'ec_lifeinvader'
 description 'LifeInvader — Werbeanzeigen-System für FiveM'
-version '1.1.23'
+version '1.1.24'
 
 shared_scripts {
     'config.lua',
